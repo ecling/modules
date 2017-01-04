05fc00d0137773b32bfbe5414219feec7525080d
ling php <php@highqb.com>
2017-01-04 18:02:49 +0800

    201701041802

---
+++ Db/Select.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Mysql数据库select语言组装
+ * 
+ * @author ling
+ * @category db
+ * 
+ */
+class Select{
+    
+}
---
+++ Helper/ArraySort.php
@@ -0,0 +1,14 @@
+<?php
+class Array_Sort{
+    /**
+     * 根据指定的顺序为数组排序
+     * 
+     * @param array $array
+     * @param array $array2
+     * @return array
+     * 
+     */
+    public function specific($array,$array2){
+        
+    }
+}
